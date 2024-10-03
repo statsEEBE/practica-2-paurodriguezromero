d37de0f585e4b2e378c9879960439c22ce4b91a7
pau.rodriguez.romero <pau.rodriguez.romero@auleseebe.upc.edu>
2024-10-03 11:28:53 +0200

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,15 @@
 #Codigo para problema 1
-
+rm(list=ls())
+rsq<-0.1923
+cor<-sqrt(rsq)
+cor
+m=0.0657
+#prediccion
+m<-0.0658
+b<-11.798
+x<-647.1
+y=m*x+b
+xpred<-0:700
+ypred=m*xpred+b
+plot(c(0,700),c(0,120))
+lines(xpred,ypred)
